72fb9a7e7bc4fd03450bd0510b9c056a8a4a7e27
Rodrigo <mitrerod@gmail.com>
2020-09-04 20:33:23 -0300

    useUnifiedTopology: true en mongoose

--- src/database.js
+++ src/database.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/productos-db-app', {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useUnifiedTopology: true
 })
     .then(db => console.log('Base de Datos conectada'))
     .catch(err => console.error(err))
