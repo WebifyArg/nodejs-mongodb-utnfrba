6751f96b08a5c7cd90d416eceef61d3fe1ef8ce7
Rodrigo <mitrerod@gmail.com>
2020-09-04 21:33:32 -0300

    Vistas creadas

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Inicio');
+    //res.send('Inicio');
+    res.render('index');
 });
 
 router.get('/nosotros', (req, res) => {
-    res.send('Nosotros');
+    //res.send('Nosotros');
+    res.render('nosotros');
 })
 
 module.exports = router;
--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/usuarios/acceder', (req, res) => {
-    res.send('Acceso a Usuarios');
+    //res.send('Acceso a Usuarios');
+    res.render('usuarios/login')
 });
 
 router.get('/usuarios/registro', (req, res) => {
-    res.send('Registro de Usuarios');
+    //res.send('Registro de Usuarios');
+    res.render('usuarios/registro')
 });
 
 module.exports = router;
